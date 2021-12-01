37bed977fb27522c1a14a8bdfbdc370be63c6bef
Ben Liset <ben.liset@coyote.com>
2021-12-01 09:38:43 -0600

    Initial Commit

---
+++ Shared/aoc2021App.swift
@@ -0,0 +1,17 @@
+//
+//  aoc2021App.swift
+//  Shared
+//
+//  Created by Ben Liset on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct aoc2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
